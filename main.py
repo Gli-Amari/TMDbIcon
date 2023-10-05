d393b02eeeb94aa520930735621f371d10f789fa
Piersilvio <p.spicoli@studenti.uniba.it>
2023-10-05 11:40:25 +0200

    caricamento altro csv di prova da biasco

--- main.py
+++ main.py
@@ -10,46 +10,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    df = pd.read_csv('./archive/tmdb_5000_movies.csv')
-    df1 = pd.read_csv('./archive/tmdb_5000_credits.csv')
-
-    df_temp = df1.apply(lambda row: pd.Series(row['cast']), axis=1).stack().reset_index(level=1,drop=True)
-    df_temp = pd.DataFrame(df_temp, columns=['cast'])
-
-    dff = df_temp.to_dict(orient = 'list')
-
-    for chiave, valore in dff.items():
-        try:
-            dff[chiave] = int(valore)
-        except (ValueError, TypeError):
-            # Se non è possibile convertire il valore in intero, mantieni la stringa originale
-            pass
-
-print(dff['cast_id'])
-
-    new_dataframe_cast = pd.DataFrame({
-        'cast_id': [di['cast_id'] for di in dff],
-        'character': [di['character'] for di in dff],
-        'credit_id': [di['credit_id'] for di in dff],
-        'gender': [di['cast_id'] for di in dff],
-        'id': [di['id'] for di in dff],
-        'name': [di['name'] for di in dff],
-        'order': [di['order'] for di in dff]
-    })
-
-    print(df_temp.info())
-    print("\n")
-    print(new_dataframe_cast)
-    print("\n")
-    print(dff)
-
-
-
-    """dataset_unione = ProcessingDf('./archive/tmdb_5000_movies.csv', './archive/tmdb_5000_credits.csv')
-    chiavi_di_unione = ['id', 'movie_id']  # Lista di chiavi per l'unione
-    risultato_unione = dataset_unione.unisci(chiavi_di_unione)
-
-    # Stampa il risultato dell'unione
-    if risultato_unione is not None:
-        print(risultato_unione.head())"""
+    dataset = ProcessingDf('./archive/tmdb_5000_movies.csv')
 
--- processing/processing.py
+++ processing/processing.py
@@ -3,53 +3,7 @@ import pandas as pd
 
 class ProcessingDf:
 
-    def __init__(self, ds1, ds2):
-        self.df1 = ds1
-        self.df2 = ds2
+    def __init__(self, dataset):
+        self.dataset = dataset
+        df = pd.read_csv(dataset)
 
-    """
-        nel try-catch prima di mergiare devono essere sistemate le feature 
-    """
-    def mergingDf(self, chiavi):
-
-        try:
-            risultato = pd.merge(self.df1, self.df2,
-                                 on = chiavi, how = 'inner')
-            return risultato
-        except Exception as e:
-            print(f"Si è verificato un errore durante l'unione dei dataset: {str(e)}")
-            return None
-
-    def extractComplexType(self):
-
-        df_temp = self.df2.apply(lambda row: pd.Series(row['cast']), axis = 1).stack().reset_index(level = 1, drop = True)
-        df_temp = pd.DataFrame(df_temp, columns = ['cast'])
-
-        dff = df_temp.to_dict(orient = 'list')
-
-        new_dataframe_cast = pd.DataFrame({
-            'cast_id': [di['cast_id'] for di in dff],
-            'character': [di['character'] for di in dff],
-            'credit_id': [di['credit_id'] for di in dff],
-            'gender': [di['cast_id'] for di in dff],
-            'id': [di['id'] for di in dff],
-            'name': [di['name'] for di in dff],
-            'order': [di['order'] for di in dff]
-        })
-
-
-
-
-
-
-    def convertObjectType(self, df):
-
-        for colonna in df.columns:
-            # Utilizza il metodo pd.to_numeric() per convertire in numerico se possibile
-            df[colonna] = pd.to_numeric(df[colonna], errors = 'ignore')
-            # Utilizza il metodo pd.to_datetime() per convertire in data se possibile
-            try:
-                df[colonna] = pd.to_datetime(df[colonna])
-            except ValueError:
-                pass  # Se non è possibile convertire in data, continua con il tipo 'object'
-        return df
